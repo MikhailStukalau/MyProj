b9d7e2aa7ffabbe8845458050c2df09c5592db1a
muxar <1>
2022-08-26 00:28:47 +0300

    homework

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.blog_index, name="blog_index"),
+    path("blog/", views.blog_index, name="blog_index"),
     path("<int:id", views.blog_detail, name="dlog_detail")
 ]
